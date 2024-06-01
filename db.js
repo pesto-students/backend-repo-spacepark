54b74c28f30eaacf690fb04b745282bea8d9b00d
Prachi-2002 <prachimadame01@gmail.com>
2024-06-01 22:42:05 +0530

    updated

--- db.js
+++ db.js
@@ -9,12 +9,7 @@ const config = require('./config/config');
 // Initialize Sequelize with the connection URL
 const sequelize = new Sequelize(process.env.DB_CONNECTION_URL, {
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true, // Force SSL/TLS connections
-      rejectUnauthorized: false, // For development purposes only; set to true in production
-    },
-  },
+  dialectOptions:config.development.dialectOptions,
 });
 
 // Test the database connection
